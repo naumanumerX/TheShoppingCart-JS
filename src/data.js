e11daae9a76c3f4b3fb7441ba4ac63feac37f2fd
Nauman <naumanumer10@outlook.com>
2023-09-10 12:32:46 +0100

    Items Data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+   
+let itemsData=[
+    {
+        id:1,
+        name:"Casual Shirt",
+        price:"£25",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-1.webp"
+    },
+    {
+        id:2,
+        name:"Regular Shirt",
+        price:"£19.99",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-2.webp"
+    },
+    {
+        id:3,
+        name:"Dress Shirt",
+        price:"£33",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-3.webp"
+    },
+    {
+        id:4,
+        name:"Casual Shirt",
+        price:"£27.99",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-4.webp"
+    },
+    ]
