ace94d3b5d2f67ec428226f9e1899bf66dc0e8c9
Nauman <naumanumer10@outlook.com>
2023-09-11 15:06:26 +0100

    Adding More Data

--- src/data.js
+++ src/data.js
@@ -28,4 +28,59 @@ let itemsData=[
         desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
         img: "images/img-4.webp"
     },
+    {
+        id:5,
+        name:"Casual Shirt",
+        price:"£25",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-1.webp"
+    },
+    {
+        id:6,
+        name:"Regular Shirt",
+        price:"£19.99",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-2.webp"
+    },
+    {
+        id:7,
+        name:"Dress Shirt",
+        price:"£33",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-3.webp"
+    },
+    {
+        id:8,
+        name:"Casual Shirt",
+        price:"£27.99",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-4.webp"
+    }, {
+        id:9,
+        name:"Casual Shirt",
+        price:"£25",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-1.webp"
+    },
+    {
+        id:10,
+        name:"Regular Shirt",
+        price:"£19.99",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-2.webp"
+    },
+    {
+        id:11,
+        name:"Dress Shirt",
+        price:"£33",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-3.webp"
+    },
+    {
+        id:12,
+        name:"Casual Shirt",
+        price:"£27.99",
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing. ",
+        img: "images/img-4.webp"
+    },
     ]
